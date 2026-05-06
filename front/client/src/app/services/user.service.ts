cca645a15f0dfc2cd01b2302aa2834d4320debc8
Esra <138771047+esraahsin@users.noreply.github.com>
2026-05-06 21:50:28 +0100

    Add backend and frontend to monorepo

---
+++ front/client/src/app/services/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export interface User {
+  id?: number;
+  name: string;
+  email: string;
+  created_at?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+private apiUrl = `${(window as any).__API_URL__ || 'http://localhost:3000'}/api/users`;
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/${user.id}`, user);
+  }
+
+  deleteUser(id: number | undefined): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
